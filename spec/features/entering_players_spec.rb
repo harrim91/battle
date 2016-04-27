faef78811d3f1db5f0696a24703fbd5fdfb115a7
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-04-27 11:05:32 +0100

    Extracted sign_in_and_play helper method

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -3,11 +3,7 @@ feature "Entering players" do
   let(:player_2_name) { "Emmett" }
 
   scenario "two players enter their names" do
-    visit("/")
-    fill_in :player_1_name, with: player_1_name
-    fill_in :player_2_name, with: player_2_name
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_content("#{player_1_name} and #{player_2_name}.")
   end
 end
--- spec/features/see_HP_spec.rb
+++ spec/features/see_HP_spec.rb
@@ -2,11 +2,8 @@ feature 'Display HP' do
   let(:player_1_name) { "Michael" }
   let(:player_2_name) { "Emmett" }
 
-  scenario "display HP of player 2" do
-    visit("/")
-    fill_in :player_1_name, with: player_1_name
-    fill_in :player_2_name, with: player_2_name
-    click_button "Submit"
+  scenario "displays HP of player 2" do
+    sign_in_and_play
     expect(page).to have_content("#{player_2_name} HP: #{Battle::PLAYER_2_HP}")
   end
 
