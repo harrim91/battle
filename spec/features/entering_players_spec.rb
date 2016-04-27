09f310693797de229ca610ea801c82a2ee9cc216
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-27 10:25:55 +0100

    refactored - added session and redirect from post route

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -1,10 +1,13 @@
 feature "Entering players" do
+  let(:player_1_name) { "Michael" }
+  let(:player_2_name) { "Emmett" }
+
   scenario "two players enter their names" do
     visit("/")
-    fill_in :player1, with: "Michael"
-    fill_in :player2, with: "Lucy"
+    fill_in :player_1_name, with: player_1_name
+    fill_in :player_2_name, with: player_2_name
     click_button "Submit"
 
-    expect(page).to have_content("Welcome Michael and Lucy.")
+    expect(page).to have_content("#{player_1_name} and #{player_2_name}.")
   end
 end
