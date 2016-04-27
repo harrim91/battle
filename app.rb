a69ecb71921bb5c05233c24386033a7fb89098ec
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-04-27 12:24:01 +0100

    Extracted Player class to the model

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class Battle < Sinatra::Base
   PLAYER_2_HP = 100
@@ -10,14 +11,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     @attack = session[:attack]
     erb :play
   end
